cdb26d50ef43e5fc3fccc02a76dc7bb54899e15d
Léo Moraes <moraes.leo@gmail.com>
2022-01-07 22:30:10 -0300

    testing pages cloudflare with php

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+header("Location: https://sites.google.com/view/portaldaesperanca-org-br/")
+
+//<script type="text/javascript">location.href = 'https://sites.google.com/view/portaldaesperanca-org-br/';</script>
